5c10f1206a4379f4efb587adb60a8bb7adab706c
Yousef Fadl <78025848+Yousef697@users.noreply.github.com>
2025-04-26 22:31:39 +0300

    CP Code

--- strings.cpp
+++ strings.cpp
@@ -67,6 +67,232 @@ int KMP(vector<int> s, vector<int> pat)
 
 // ==========================================================================================
 
+// Z-Algorithm
+vector<int> z_algorithm(const string& s)
+{
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (i < r)
+            z[i] = min(r - i, z[i - l]);
+
+        while (i + z[i] < n && s[i + z[i]] == s[z[i]])
+            z[i]++;
+
+        if (i + z[i] > r)
+            l = i, r = i + z[i];
+    }
+
+    return z;
+}
+vector<int> find_pattern(const string& s, const string& t)
+{
+    string A = s + '#' + t;
+    int n = s.size(), m = t.size();
+    vector<int> Z = z_algorithm(A);
+
+    vector<int> ans;
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (Z[i + n + 1] == n)
+            ans.push_back(i);
+    }
+
+    return ans;
+}
+int distinct_substrings(const string& s)
+{
+    int n = s.size(), m = 0, ans = 0;
+    string t = "";
+
+    for (const char& c : s)
+    {
+        m++;
+        t += c;
+        int mx = 0;
+        reverse(t.begin(), t.end());
+
+        vector<int> Z = z_algorithm(t);
+        for (const int i : Z)
+            mx = max(mx, i);
+        ans += m - mx;
+
+        reverse(t.begin(), t.end());
+    }
+
+    return ans;
+}
+string period(const string& s)
+{
+    int n = s.size(), ans = 0;
+    vector<int> Z = z_algorithm(s);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i)
+            continue;
+
+        if (i + Z[i] == n)
+        {
+            ans = i;
+            break;
+        }
+    }
+
+    return s.substr(0, ans);
+}
+
+// ==========================================================================================
+
+// Hashing
+mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+int rand(int l, int r) { return uniform_int_distribution<int>(l, r)(rng); }
+
+struct Hash
+{
+    int mod, base, n;
+    vector<int> prf, suf, pows, invs;
+    vector<int> primesMod = {1000000007, 1000000009, 1000000021, 1000000033, 1000000087, 1000000093, 1000000097, 1000000103, 1000000123, 1000000181, 1000000207, 1000000223, 1000000241, 1000000271, 1000000289, 1000000297, 1000000321, 1000000349, 1000000363, 1000000403, 1000000409, 1000000411, 1000000427, 1000000433, 1000000439, 1000000447, 1000000453, 1000000459, 1000000483, 1000000513, 1000000531, 1000000579, 1000000607, 1000000613, 1000000637, 1000000663, 1000000711, 1000000753, 1000000787, 1000000801, 1000000829, 1000000861, 1000000871, 1000000891, 1000000901, 1000000919, 1000000931, 1000000933, 1000000993, 1000001011};
+
+    int fpow(int x, int p)
+    {
+        int ans = 1;
+        while (p)
+        {
+            if (p & 1)
+                ans = (1LL * ans * x) % mod;
+            p /= 2;
+            x = (1LL * x * x) % mod;
+        }
+        return ans;
+    }
+
+    Hash(const string& s)
+    {
+        int r = rand(0, 49);
+        mod = primesMod[r];
+        base = rand(2, 199);
+
+        n = s.size();
+        pows.assign(n + 5, 1);
+        invs.assign(n + 5, 1);
+
+        for (int i = 1; i < n + 5; i++)
+        {
+            pows[i] = (1LL * pows[i - 1] * base) % mod;
+            invs[i] = fpow(pows[i], mod - 2);
+        }
+
+        int value = 0, p = 0;
+        prf.push_back(0);
+        for (int i = 0; i < n; i++)
+        {
+            char c = s[i];
+            value = (value + 1LL * (c - 'a' + 1) * pows[p++]) % mod;
+            prf.push_back(value);
+        }
+        prf.push_back(0);
+
+        value = 0, p = 0;
+        suf.push_back(0);
+        for (int i = n - 1; i >= 0; i--)
+        {
+            char c = s[i];
+            value = (value + 1LL * (c - 'a' + 1) * pows[p++]) % mod;
+            suf.push_back(value);
+        }
+    }
+
+    int substring(int l, int r, int who = 0) /// 1-based
+    {
+        int cand;
+        if (who == 0) /// prfix
+        {
+            cand = (prf[r] - prf[l - 1]) % mod;
+            cand = (cand + mod) % mod;
+            cand = (1LL * cand * invs[l]) % mod;
+        }
+        else /// suffix
+        {
+            int ll = l, rr = r;
+            l = n - rr + 1, r = n - ll + 1;
+            cand = (suf[r] - suf[l - 1]) % mod;
+            cand = (cand + mod) % mod;
+            cand = (1LL * cand * invs[l]) % mod;
+        }
+        return cand;
+    }
+
+    bool palindrome(int l, int r)
+    {
+        return substring(l, r, 0) == substring(l, r, 1);
+    }
+};
+
+// ==========================================================================================
+
+// Trie
+const int K = 26;
+struct Trie
+{
+    struct node
+    {
+        int next[K];
+        bool leaf = 0;
+
+        node() { fill(begin(next), end(next), -1); }
+    };
+    vector<node> tree;
+
+    Trie() { tree.push_back(node()); }
+
+    void add(const string& s)
+    {
+        int v = 0;
+        for (const char& c : s)
+        {
+            if (tree[v].next[c - 'a'] == -1)
+            {
+                tree[v].next[c - 'a'] = tree.size();
+                tree.push_back(node());
+            }
+            v = tree[v].next[c - 'a'];
+        }
+        tree[v].leaf = true;
+    }
+
+    int serach_word(const string& s)
+    {
+        int v = 0;
+        for (const char& c : s)
+        {
+            if (tree[v].next[c - 'a'] == -1)
+                return -1;
+
+            v = tree[v].next[c - 'a'];
+        }
+        return (tree[v].leaf == 1 ? 1 : -1);
+    }
+
+    int serach_prefix(const string& s)
+    {
+        int v = 0;
+        for (const char& c : s)
+        {
+            if (tree[v].next[c - 'a'] == -1)
+                return -1;
+
+            v = tree[v].next[c - 'a'];
+        }
+        return 1;
+    }
+};
+
+// ==========================================================================================
+
 int32_t main()
 {
     ios::sync_with_stdio(false);
