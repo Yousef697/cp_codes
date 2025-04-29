c3167b9f9cf2f036f81c923ee5f29f97f52dd5e9
Yousef Fadl <78025848+Yousef697@users.noreply.github.com>
2025-04-30 00:08:18 +0300

    CP Codes

--- strings.cpp
+++ strings.cpp
@@ -292,6 +292,87 @@ struct Trie
 
 // ==========================================================================================
 
+// Recursive Trie
+const int K = 2;
+struct Trie
+{
+    vector<int> leaf;
+    vector<int> cnts;
+    vector<vector<int>> tree;
+
+    Trie()
+    {
+        leaf.push_back(0);
+        cnts.push_back(0);
+        tree.push_back(vector<int>(2, 0));
+    }
+
+    void insert(const string& s, int v = 0, int i = 0)
+    {
+        if (i == s.size())
+        {
+            cnts[v]++;
+            leaf[v] = 1;
+            return;
+        }
+
+        if (!tree[v][s[i] - '0'])
+        {
+            tree[v][s[i] - '0'] = tree.size();
+
+            leaf.push_back(0);
+            cnts.push_back(0);
+            tree.push_back(vector<int>(2, 0));
+        }
+
+        int next = tree[v][s[i] - '0'];
+        insert(s, next, i + 1);
+
+        cnts[v]++;
+    }
+
+    void erase(const string& s, int v = 0, int i = 0)
+    {
+        if (i == s.size())
+        {
+            cnts[v]--;
+            if (cnts[v] == 0)
+                leaf[v] = 0;
+            return;
+        }
+
+        assert(tree[v][s[i] - '0']);
+        int next = tree[v][s[i] - '0'];
+        erase(s, next, i + 1);
+
+        cnts[v]--;
+        if (cnts[next] == 0)
+            tree[v][s[i] - '0'] = 0;
+    }
+
+    int serach_word(const string& s, int v = 0, int i = 0)
+    {
+        if (i == s.size())
+            return leaf[v];
+
+        if (!tree[v][s[i] - '0'])
+            return -1;
+        return serach_word(s, tree[v][s[i] - '0'], i + 1);
+    }
+
+    int serach_prefix(const string& s, int v = 0, int i = 0)
+    {
+        if (i == s.size())
+            return 1;
+
+        if (!tree[v][s[i] - '0'])
+            return -1;
+        return serach_prefix(s, tree[v][s[i] - '0'], i + 1);
+    }
+};
+
+// ==========================================================================================
+
 // Manacher
 vector<int> manacher(const string& s)
 {
