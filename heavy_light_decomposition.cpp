e1d3c7c7cea477bb1cb29805fbbe1988e3f6de4c
Yousef Fadl <78025848+Yousef697@users.noreply.github.com>
2025-06-15 16:23:11 +0300

    Update heavy_light_decomposition.cpp

--- heavy_light_decomposition.cpp
+++ heavy_light_decomposition.cpp
@@ -2,6 +2,102 @@
 
 using namespace std;
 
+struct HLD {
+    int n, chain_counter, flat_index;
+    vector<int> tree_parent; // parent of node i in the tree
+    vector<int> nodes; // number of node in the subtree of node i
+    vector<int> leader; // leader of the chain that contain node i
+    vector<int> chain_number; // chain number of each node
+    vector<int> index_in_chain; // index of each node in its chain
+    vector<int> index_in_flat; // index of each node in the flattened tree
+    vector<int> flat; // flattened tree
+    vector<vector<int>> adj; // adjacency list of each node
+    vector<vector<int>> chains; // nodes of each chain
+
+    HLD(const vector<vector<int>>& _adj) {
+
+        adj = _adj;
+        flat_index = 0;
+        chain_counter = 0;
+        n = adj.size() - 1;
+
+        tree_parent = nodes = leader = chain_number = index_in_chain = index_in_flat =
+            vector<int> (n + 1);
+        chains.resize(n + 1);
+
+        leader[1] = 1;
+
+        dfs_nodes(1, 0);
+        dfs_leader(1, 0);
+        get_chains(1, 0, 1);
+
+        flat.push_back(0);
+        for (int i = 1; i <= n; i++) {
+            if (chains[i].empty())
+                continue;
+
+            chain_counter++;
+            for (auto v : chains[i]) {
+                flat.push_back(v);
+                index_in_flat[v] = ++flat_index;
+            }
+        }
+    }
+
+    void dfs_nodes(int u, int p) {
+
+        nodes[u] = 1;
+        for (auto v : adj[u]) {
+            if (v == p)
+                continue;
+
+            dfs_nodes(v, u);
+            tree_parent[v] = u;
+            nodes[u] += nodes[v];
+        }
+    }
+
+    void dfs_leader(int u, int p) {
+
+        int mx = 0, cnt = 0;
+        for (auto v : adj[u])
+            if (v != p)
+                mx = max(mx, nodes[v]);
+
+        for (auto v : adj[u]) {
+            if (v == p)
+                continue;
+
+            if (nodes[v] == mx && !cnt)
+                leader[v] = leader[u], cnt = 1;
+            else
+                leader[v] = v;
+            dfs_leader(v, u);
+        }
+    }
+
+    void get_chains(int u, int p, int index) {
+
+        index_in_chain[u] = index;
+        chains[leader[u]].push_back(u);
+
+        for (auto v : adj[u]) {
+            if (v == p)
+                continue;
+
+            if (leader[v] == leader[u])
+                get_chains(v, u, index + 1);
+            else
+                get_chains(v, u, 1);
+            /// get_chains(v, u, (leader[v] == leader[u]) * index + 1);
+        }
+    }
+
+    int query(int u, int v) {
+        return 0;
+    }
+};
+
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
@@ -9,8 +105,8 @@ int32_t main() {
     int n;
     cin >> n;
 
-    vector<int> tree_parent(n + 1); // parent of node i in the tree
-    vector<vector<int>> adj(n + 1); // adjacency list of each node
+    vector<vector<int>> adj(n + 1);
+
     for (int i = 1; i <= n - 1; i++) {
         int u, v;
         cin >> u >> v;
@@ -19,74 +115,24 @@ int32_t main() {
         adj[v].push_back(u);
     }
 
-    vector<int> dp_nodes(n + 1); // number of node in the subtree of node i
-    function<void(int, int)> dfs_nodes = [&](int u, int p) {
+    HLD hld(adj);
 
-        dp_nodes[u] = 1;
-        for (auto v : adj[u]) {
-            if (v == p)
-                continue;
-
-            dfs_nodes(v, u);
-            tree_parent[v] = u;
-            dp_nodes[u] += dp_nodes[v];
-        }
-    };
-    dfs_nodes(1, 0);
-
-    vector<int> leader(n + 1); leader[1] = 1; // leader on the chain that contain node i
-    function<void(int, int)> dfs_leader = [&](int u, int p) {
-
-        int mx = 0, cnt = 0;
-        for (auto v : adj[u])
-            if (v != p)
-                mx = max(mx, dp_nodes[v]);
-
-        for (auto v : adj[u]) {
-            if (v == p)
-                continue;
-
-            if (dp_nodes[v] == mx && !cnt)
-                leader[v] = leader[u], cnt = 1;
-            else
-                leader[v] = v;
-            dfs_leader(v, u);
-        }
-    };
-    dfs_leader(1, 0);
-
-    int chain_counter = 1;
-    vector<vector<int>> chains(n + 1);
-    vector<int> chain_number(n + 1), rank_in_chain(n + 1);
-    function<void(int, int, int)> get_chains = [&](int u, int p, int rank) {
-
-        rank_in_chain[u] = rank;
-        chains[leader[u]].push_back(u);
-
-        for (auto v : adj[u]) {
-            if (v == p)
-                continue;
-
-            if (leader[v] == leader[u])
-                get_chains(v, u, rank + 1);
-            else
-                get_chains(v, u, 1), chain_counter++;
-            /// get_chains(v, u, (leader[v] == leader[u]) * rank + 1);
-        }
-    };
-    get_chains(1, 0, 1);
-
-    // printing the chains
-    int j = 0;
     for (int i = 1; i <= n; i++) {
-        if (chains[i].empty())
+        if (hld.chains[i].empty())
             continue;
 
-        cout << "Chain " << ++j << ": ";
-        for (auto v : chains[i])
+        for (auto v : hld.chains[i]) {
             cout << v << ", ";
+        }
         cout << "\n";
     }
 
+    for (auto i : hld.flat)
+        cout << i << " ";
+    cout << "\n";
+    for (auto i : hld.index_in_flat)
+        cout << i << " ";
+
     return 0;
 }
+
