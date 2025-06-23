046ddb0b1eff49c934ce997ab37e68fea959d9f1
Yousef Fadl <78025848+Yousef697@users.noreply.github.com>
2025-06-23 08:17:03 +0300

    Update bigint.cpp

--- bigint.cpp
+++ bigint.cpp
@@ -1,5 +1,6 @@
 // https://vjudge.net/problem/HDU-6222
 #include <bits/stdc++.h>
+#define int long long
 
 using namespace std;
 
@@ -8,61 +9,94 @@ struct bigint {
     int sign = 1;
     vector<int> digits;
 
-    bigint() {}
+    bigint() {
+        sign = 1;
+        digits.push_back(0);
+    }
     bigint(int v) {
         digits.clear();
         if (v < 0)
             sign = -1, v *= -1;
-
+        else
+            sign = 1;
         while (v)
             digits.push_back(v % 10), v /= 10;
+        normalize(digits);
     }
     bigint(string s, int _sign = 1) {
         if (s.size() && s[0] == '-')
             sign = -1, s = s.substr(1);
+        if (s.size() && s[0] == '+')
+            sign = +1, s = s.substr(1);
         sign *= _sign;
 
         reverse(s.begin(), s.end());
         for (auto i : s)
             digits.push_back(i - '0');
+        normalize(digits);
     }
     bigint(const vector<int>& v, int _sign = 1) {
         digits = v;
         sign = _sign;
+        normalize(digits);
+    }
+
+    void normalize(vector<int>& num) const {
+        int n = num.size();
+        for (int i = 0; i < n - 1; i++) {
+            if (num[i] < 0) {
+                int b = (abs(num[i]) + 9) / 10;
+                num[i] += 10 * b;
+                num[i + 1] -= b;
+            }
+            else {
+                num[i + 1] += num[i] / 10;
+                num[i] %= 10;
+            }
+        }
+        while (num.size() && num.back() >= 10) {
+            int x = num.back() / 10;
+            num.back() %= 10;
+            num.push_back(x);
+        }
+        while (num.size() && num.back() == 0)
+            num.pop_back();
+        if (num.empty())
+            num.push_back(0);
     }
 
     int size() const {
         return digits.size();
     }
     int is_zero() const {
-        return digits.empty();
-    }
-    void print() {
-        if (size() == 0) {
-            cout << 0;
-            return;
-        }
-        if (sign == -1)
-            cout << "-";
-        for (int i = size() - 1; i >= 0; i--)
-            cout << digits[i];
+        return size() == 1 && digits[0] == 0;
     }
 
     bigint& operator=(int v) {
         digits.clear();
+        if (v < 0)
+            sign = -1, v = -v;
+        else
+            sign = 1;
         while (v)
             digits.push_back(v % 10), v /= 10;
         return *this;
     }
-    bigint& operator=(const bigint& other) {
-        digits = other.digits;
-        return *this;
-    }
     bigint& operator=(string s) {
         digits.clear();
+        if (s.size() && s[0] == '-')
+            sign = -1, s = s.substr(1);
+        if (s.size() && s[0] == '+')
+            sign = +1, s = s.substr(1);
+
         reverse(s.begin(), s.end());
         for (auto i : s)
             digits.push_back(i - '0');
+        return *this;
+    }
+    bigint& operator=(const bigint& other) {
+        digits = other.digits;
+        sign = other.sign;
         return *this;
     }
 
@@ -70,34 +104,41 @@ struct bigint {
         if (sign != other.sign)
             return sign < other.sign;
         if (size() != other.size())
-            return (size() < other.size());
+            return (sign == 1 ? size() < other.size() : size() > other.size());
         for (int i = size() - 1; i >= 0; i--)
             if (digits[i] != other.digits[i])
-                return (digits[i] < other.digits[i]);
+                return (sign == 1 ? digits[i] < other.digits[i] : digits[i] > other.digits[i]);
         return false;
     }
     bool operator>(const bigint& other) const {
         if (sign != other.sign)
             return sign > other.sign;
         if (size() != other.size())
-            return (size() > other.size());
+            return (sign == 1 ? size() > other.size() : size() < other.size());
         for (int i = size() - 1; i >= 0; i--)
             if (digits[i] != other.digits[i])
-                return (digits[i] > other.digits[i]);
+                return (sign == 1 ? digits[i] > other.digits[i] : digits[i] < other.digits[i]);
         return false;
     }
     bool operator==(const bigint& other) const {
-        if (sign != other.sign)
-            return false;
-        if (digits.size() != other.digits.size())
-            return false;
-        for (int i = size() - 1; i >= 0; i--)
-            if (digits[i] != other.digits[i])
-                return (false);
-        return true;
+        return !(*this > other || *this < other);
+    }
+    bool operator<=(const bigint& other) const {
+        return *this < other || *this == other;
+    }
+    bool operator>=(const bigint& other) const {
+        return *this > other || *this == other;
+    }
+
+    bigint operator-() const {
+        bigint res = *this;
+        if (!is_zero())
+            res.sign *= -1;
+        return res;
     }
 
     bigint operator+(const bigint& other) const {
+
         int n = max(size(), other.size());
         vector<int> res(n);
 
@@ -106,20 +147,17 @@ struct bigint {
                 res[i] += digits[i];
             for (int i = 0; i < other.size(); i++)
                 res[i] += other.digits[i];
-            for (int i = 0; i < n - 1; i++)
-                res[i + 1] += res[i] / 10, res[i] %= 10;
-            if (res[n - 1] >= 10)
-                res.push_back(res[n - 1] / 10), res[n - 1] %= 10;
-            while (res.size() && res.back() == 0)
-                res.pop_back();
-            return bigint(res);
+            normalize(res);
+            return bigint(res, sign);
         }
-        else if (sign == 1 && other.sign == -1) {
-            return *this - other;
+        if (sign == 1 && other.sign == -1) {
+            bigint tmp = other;
+            tmp.sign *= -1;
+            return *this - tmp;
         }
-        else {
-            return other - *this;
-        }
+        bigint tmp = *this;
+        tmp.sign = -1;
+        return other - tmp;
     }
     bigint operator+=(const bigint& other) {
         *this = *this + other;
@@ -133,16 +171,8 @@ struct bigint {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 res[i + j] += digits[i] * other.digits[j];
-        for (int i = 0; i < n + m - 1; i++)
-            res[i + 1] += res[i] / 10, res[i] %= 10;
-        while (res.back() >= 10) {
-            int x = res.back();
-            res.back() %= 10;
-            res.push_back(x / 10);
-        }
-        while (res.size() && res.back() == 0)
-            res.pop_back();
-        return bigint(res);
+        normalize(res);
+        return bigint(res, sign * other.sign);
     }
     bigint operator*=(const bigint& other) {
         *this = *this * other;
@@ -150,8 +180,6 @@ struct bigint {
     }
 
     bigint operator-(const bigint& other) const {
-
-        int sign = 1;
         int n = max(size(), other.size());
         vector<int> a = digits, b = other.digits;
         while (a.size() < n)
@@ -159,20 +187,20 @@ struct bigint {
         while (b.size() < n)
             b.push_back(0);
 
-        if (*this < other)
-            sign = -1, swap(a, b);
+        if (sign != other.sign) {
+            for (int i = 0; i < n; i++)
+                a[i] += b[i];
+            normalize(a);
+            return bigint(a, sign);
+        }
 
+        if (*this < other) {
+            bigint tmp = other - *this;
+            return bigint(tmp.digits, -1);
+        }
         for (int i = 0; i < n; i++)
             a[i] -= b[i];
-
-        for (int i = 0; i < n - 1; i++) {
-            while (a[i] < 0)
-                a[i] += 10, a[i + 1]--;
-        }
-        while (a.back() < 0)
-            a.back() += 10;
-        while (a.size() && a.back() == 0)
-            a.pop_back();
+        normalize(a);
         return bigint(a, sign);
     }
     bigint operator-=(const bigint& other) {
@@ -199,7 +227,7 @@ void println(const bigint& bi) {
         cout << '-';
     for (int i = bi.size() - 1; i >= 0; i--)
         cout << bi.digits[i];
-    cout << "\n";
+    cout << endl;
 }
 
 int32_t main() {
@@ -207,16 +235,12 @@ int32_t main() {
     cin.tie(nullptr), cout.tie(nullptr);
 
     vector<bigint> nums;
-    nums.push_back(bigint("4"));
-    nums.push_back(bigint("14"));
-    nums.push_back(bigint("52"));
+    nums.push_back(bigint(4));
+    nums.push_back(bigint(14));
 
-    for (int i = 3; i <= 60; i++) {
-        bigint res("1");
-        res *= nums[i - 1];
-        res *= bigint("4");
-        res -= nums[i - 2];
-        nums.push_back(res);
+    for (int i = 2; i <= 60; i++) {
+        int n = nums.size();
+        nums.push_back(bigint(4) * nums[n - 1] - nums[n - 2]);
     }
 
     int t;
@@ -228,8 +252,8 @@ int32_t main() {
 
         bigint n(s);
         for (int i = 0; i < nums.size(); i++) {
-            if (n < nums[i] || nums[i] == n) {
-                print(nums[i]);
+            if (n <= nums[i]) {
+                println(nums[i]);
                 break;
             }
         }
