5a36105b14bc44f0906bca76d767d6fbd8c66cfa
Yousef Fadl <78025848+Yousef697@users.noreply.github.com>
2025-11-11 16:31:05 +0200

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -592,25 +592,26 @@ pair<int, int> lcs(string& s, string& t) {
 
 // ==========================================================================================
 
+const int K = 26;
+struct vertex {
+    char ch;
+    bool leaf = false;
+    int next[K], parent, fail, exit, id;
+
+    vertex(int _p = -1, char _ch = '$') {
+        ch = _ch;
+        parent = _p;
+        fail = exit = 0;
+        memset(next, -1, sizeof next);
+    }
+};
 struct aho_corasick {
-    const int K = 26;
-    struct vertex {
-        char ch;
-        bool leaf = false;
-        int next[K], parent, fail, cnt;
-
-        vertex(int _p = -1, char _ch = '$') : parent(_p), ch(_ch) {
-            fail = cnt = 0;
-            memset(next, -1, sizeof next);
-        }
-    };
-
     int time = -1;
     vector<vertex> tree;
     vector<int> in, out;
     aho_corasick() { tree.emplace_back(); }
 
-    void insert(const string &s, int idx) {
+    void insert(const string &s, int id = 0) {
         int u = 0;
         for (auto &c: s) {
             int v = c - 'a';
@@ -620,7 +621,7 @@ struct aho_corasick {
             }
             u = tree[u].next[v];
         }
-        tree[u].cnt++;
+        tree[u].id = id;
         tree[u].leaf = true;
     }
     void dfs(int u) {
@@ -660,22 +661,30 @@ struct aho_corasick {
                 return tree[u].next[x];
             return u;
         };
-
         while (!q.empty()) {
             int u = q.front();
             q.pop();
             tree[u].fail = compute_failure(u);
-            if (~tree[u].parent)
-                tree[u].cnt += tree[ tree[u].parent ].cnt;
             for (int i = 0; i < K; i++) {
                 if (tree[u].next[i] != -1) {
                     q.push(tree[u].next[i]);
                 }
             }
         }
+        for (int i = 0; i < tree.size(); i++) {
+            int j = tree[i].fail;
+            while (j) {
+                if (tree[j].leaf) {
+                    break;
+                }
+                j = tree[j].fail;
+            }
+            tree[i].exit = j;
+        }
     }
     void print() {
         // tree
+        cout << "tree:\n";
         for (int i = 0; i < tree.size(); i++) {
             for (int j = 0; j < K; j++) {
                 if (tree[i].next[j] != -1) {
@@ -687,16 +696,71 @@ struct aho_corasick {
             }
         }
         // failure
+        cout << "failure\n";
         for (int i = 0; i < tree.size(); i++) {
             cout << i << " ";
             cout << tree[i].fail << "\n";
         }
+        // exit
+        cout << "exit:\n";
+        for (int i = 0; i < tree.size(); i++) {
+            if (tree[i].exit != 0) {
+                cout << i << " ";
+                cout << tree[i].exit << "\n";
+            }
+        }
+        cout << "\n";
     }
 
-    int fail(int u) { return tree[u].fail; }    
-    int next(int u, char c) { return tree[u].next[c]; }
+    int fail(int u) { return tree[u].fail; }
+    int next(int u, int c) { return tree[u].next[c]; }
     vertex operator[](int u) { return tree[u]; }
 };
+long long find_strings(const vector<string>& pats, const string& s) {
+    // vector<string> words = {
+    //     "abba", "baab", "aba", "aabb"
+    // };
+    // string t = "abbaabaabab";
+    // cout << find_strings(words, t) << endl;
+
+    int cnt = 0;
+    aho_corasick aho;
+    for (auto& p: pats) {
+        aho.insert(p, cnt++);
+    }
+    aho.build_aho();
+
+    int u = 0, ind = -1;
+    vector<array<int, 3>> ret; // which string, which index, which node
+    for (auto& c : s) {
+        ind++;
+        int x = c - 'a';
+        if (aho.tree[u].next[x] != -1) {
+            u = aho.next(u, x);
+        }
+        else {
+            while (u) {
+                u = aho.fail(u);
+                if (aho.next(u, x) != -1) {
+                    break;
+                }
+            }
+            if (aho.next(u, x) != -1) {
+                u = aho.next(u, x);
+            }
+        }
+
+        if (aho[u].leaf)
+            ret.push_back({aho[u].id, ind, u});
+        int v = aho[u].exit;
+        while (v) {
+            ret.push_back({aho[v].id, ind, v});
+            v = aho[v].exit;
+        }
+    }
+    return ret.size();
+}
+
 
 int32_t main() {
     ios::sync_with_stdio(false);
