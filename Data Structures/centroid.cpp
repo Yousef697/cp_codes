7c401d3f10f6a0a4e7a04c007553e48394d97113
Yousef Fadl <78025848+Yousef697@users.noreply.github.com>
2026-04-26 16:08:16 +0300

    Update centroid.cpp

--- Data Structures/centroid.cpp
+++ Data Structures/centroid.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 using ll = long long;
-const int N = 2e5 + 5;
+const int inf = 1e9 + 5;
 
 struct centroid_decomposition {
     // centroid variables
@@ -12,17 +12,16 @@ struct centroid_decomposition {
     vector<vector<int>> adj;
 
     // problem-specific variables
-    int k;
-    ll ans;
-    vector<int> cur;
+    vector<int> ans;
+    vector<vector<pair<int, int>>> up;
 
-    centroid_decomposition(int n, int k): n(n), k(k) {
+    centroid_decomposition(int n): n(n) {
         sz.assign(n + 1, 0);
         blocked.assign(n + 1, false);
         adj.assign(n + 1, {});
 
-        ans = 0;
-        cur.assign(n + 1, 0);
+        ans.assign(n + 1, inf);
+        up.assign(n + 1, {});
     }
 
     void add_edge(int u, int v) {
@@ -48,36 +47,16 @@ struct centroid_decomposition {
         return u;
     }
 
-    void get_dist(int u, int p, int d, vector<int>& dist) {
-        dist.push_back(d);
-        if (d == k) return;
+    void get_dist(int u, int p, int d, int cent) {
+        up[u].push_back({cent, d});
         for (auto& v : adj[u]) {
             if (v == p || blocked[v]) continue;
-            get_dist(v, u, d + 1, dist);
+            get_dist(v, u, d + 1, cent);
         }
     }
 
     void process_centroid(int centroid) {
-
-        cur[0] = 1;
-        vector<int> all_dist;
-        for (auto& v : adj[centroid]) {
-            if (blocked[v]) continue;
-
-            vector<int> dist;
-            get_dist(v, centroid, 1, dist);
-            for (auto& d : dist) {
-                ans += cur[k - d];
-            }
-            for (auto& d : dist) {
-                cur[d]++;
-                all_dist.push_back(d);
-            }
-        }
-        for (auto& d : all_dist) {
-            cur[d] = 0;
-        }
-        cur[0] = 0;
+        get_dist(centroid, -1, 0, centroid);
     }
 
     void solve(int u = 1) {
@@ -92,17 +71,30 @@ struct centroid_decomposition {
             solve(v);
         }
     }
+
+    void update(int u) {
+        for (auto& [cent, dist] : up[u]) {
+            ans[cent] = min(ans[cent], dist);
+        }
+    }
+
+    int query(int u) {
+        int ret = ans[u];
+        for (auto& [cent, dist] : up[u]) {
+            ret = min(ret, ans[cent] + dist);
+        }
+        return ret;
+    }
 };
 
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr), cout.tie(nullptr);
+    
+    int n, m;
+    cin >> n >> m;
 
-    int n, k;
-    cin >> n >> k;
-
-    centroid_decomposition cd(n, k);
-
+    centroid_decomposition cd(n);
     for (int i = 1; i < n; i++) {
         int u, v;
         cin >> u >> v;
@@ -110,7 +102,15 @@ int32_t main() {
     }
 
     cd.solve();
-    cout << cd.ans << "\n";
+    cd.update(1);
 
+    while (m--) {
+        int t, u;
+        cin >> t >> u;
+
+        if (t == 1) cd.update(u);
+        else cout << cd.query(u) << "\n";
+    }
+    
     return 0;
 }
