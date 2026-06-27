f2aeea436d43eaf82bb8ff23b99600731a24583a
Yousef Fadl <78025848+Yousef697@users.noreply.github.com>
2026-06-27 17:23:30 +0300

    Update common_codes.cpp

--- common_codes.cpp
+++ common_codes.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 using ll = long long;
+using ld = long double;
 
 // Ordered Set / Ordered Multiset
 #include <ext/pb_ds/assoc_container.hpp>
@@ -61,6 +62,28 @@ void ranges_union(vector<pair<int, int>>& ranges) {
     ranges = cur;
 }
 
+// fraction struct
+struct fraction {
+    ll a, b;
+ 
+    fraction() : a(0), b(1) {}
+    fraction(ll x) : a(x), b(1) {}
+    fraction(ll x, ll y) : a(x), b(y) { norm(); }
+ 
+    ll gcd(ll x, ll y) { return y == 0 ? x : gcd(y, x % y); }
+    void norm() { ll g = gcd(a, b); a /= g, b /= g; }
+};
+fraction add(const fraction& n, const fraction& m) { return {n.a * m.b + n.b * m.a, n.b * m.b}; }
+fraction sub(const fraction& n, const fraction& m) { return {n.a * m.b - n.b * m.a, n.b * m.b}; }
+fraction mul(const fraction& n, const fraction& m) { return {n.a * m.a, n.b * m.b}; }
+fraction div(const fraction& n, const fraction& m) { return {n.a * m.b,  n.b * m.a}; }
+fraction abs(const fraction& n) { return {abs(n.a), abs(n.b)}; }
+int cmp(const fraction& n, const fraction& m) {
+    if (n.a == m.a && n.b == m.b) return 0;
+    return n.a * m.b < m.a * n.b ? -1 : 1;
+}
+void println(const fraction& n) { cout << n.a << "/" << n.b << "\n"; }
+void print(fraction& n) { cout << n.a << "/" << n.b; }
 
 int32_t main() {
     ios::sync_with_stdio(false);
